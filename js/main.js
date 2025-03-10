a50d599672846624aa5f66c59b056322cbe5f7d9
Luong Tuan Kiet <kietboy8@gmail.com>
2025-03-10 22:54:27 +0700

    add func addTabEvent and add 3 images

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
 /*Name this external file gallery.js*/
 
 function upDate(previewPic){
-    /* In this function you should 
-       1) change the url for the background image of the div with the id = "image" 
-       to the source file of the preview image
-       
-       2) Change the text  of the div with the id = "image" 
-       to the alt text of the preview image 
-       */
-      let imgDiv = document.getElementById("image");
-      imgDiv.style.backgroundImage = "url('" + previewPic.src + "')";
-      imgDiv.innerHTML = previewPic.alt;
-     
-       }
-   
-       function unDo(){
-        /* In this function you should 
-       1) Update the url for the background image of the div with the id = "image" 
-       back to the orginal-image.  You can use the css code to see what that original URL was
-       
-       2) Change the text  of the div with the id = "image" 
-       back to the original text.  You can use the html code to see what that original text was
-       */
-       let imgDiv = document.getElementById("image");
-       imgDiv.style.backgroundImage = "";
-       imgDiv.innerHTML = "Hover over an image below to display here.";
-       }
+    let imgDiv = document.getElementById("image");
+    imgDiv.style.backgroundImage = `url('${previewPic.src}')`;
+    imgDiv.innerHTML = previewPic.alt;
+}
+
+function unDo(){
+    let imgDiv = document.getElementById("image");
+    imgDiv.style.backgroundImage = "";
+    imgDiv.innerHTML = "Hover over an image below to display here.";
+}
+
+function addTabEvent() {
+    var imgTags = document.querySelectorAll(".preview");
+    for(const img of imgTags) {
+        img.setAttribute("tabindex", "0");
+        
+        if(!img.dataset.eventAdded) {
+            img.addEventListener("focus", () => {
+                upDate(img);
+            });
+            img.addEventListener("blur", () => {
+                unDo();
+            });
+            img.dataset.eventAdded = "true";
+        }
+    }
+}
